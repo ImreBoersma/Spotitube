e61d449dfdedd675af5cd450a5d910200d47aac2
Imre Boersma <imre.boersma@gmail.com>
2021-09-27 10:33:33 +0200

    Initial commit

---
+++ spotitube/src/test/java/nl/imreboersma/AppTest.java
@@ -0,0 +1,13 @@
+package nl.imreboersma;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
